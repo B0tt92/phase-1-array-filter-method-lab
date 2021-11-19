ff91a6455073f709e51e3e6615973b4b72ba39f5
+branden <+branden_ott@outlook.com+>
2021-11-18 19:16:01 -0500

    commit tree 91fcaf71ecdca5f49325dd0d8566b8d457267b2e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( match =>
+      match.toLowerCase() === string.toLowerCase()
+    )
+  }
+
+
+  function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  } // not mine. from sol branch. need to review 
